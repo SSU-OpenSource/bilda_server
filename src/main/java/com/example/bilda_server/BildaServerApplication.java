b81415f5af317b2af7c3cc0ec88a71e324c3d17e
gundorit <packdev937@gmail.com>
2023-11-13 14:04:10 +0900

    initial commit

---
+++ src/main/java/com/example/bilda_server/BildaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilda_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BildaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BildaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bilda_server/BildaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilda_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BildaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
