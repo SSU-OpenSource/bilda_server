db834992dba3f5725836bdaae9a8fa59fbd88a4c
David <isdnhk@naver.com>
2023-11-14 16:36:44 +0900

    [add] Subject, Team, User domain추가

---
+++ src/main/java/com/example/bilda_server/domain/User.java
@@ -0,0 +1,32 @@
+package com.example.bilda_server.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+
+    private String email;
+    private String password;
+    private String nickname;
+    private String studentId;
+
+
+    @Enumerated(EnumType.STRING)
+    private Department department;
+
+    //private Page myPage;
+
+    @OneToMany(mappedBy = "user")
+    private List<Team> teams;
+}
