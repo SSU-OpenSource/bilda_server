59fec2ac6cef0771562ac04ce369d3cf1ef34a49
David <isdnhk@naver.com>
2023-11-15 11:02:29 +0900

    [add] User에 page Entity 추가

--- src/main/java/com/example/bilda_server/domain/User.java
+++ src/main/java/com/example/bilda_server/domain/User.java
@@ -25,7 +25,11 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Department department;
 
-    //private Page myPage;
+    //page 엔티티에 정의된 User 객체에 대한 참조를 나타낸다.
+    //cascade 는 User엔티티와 관련된 Page엔티티에 대한 영속성 관리 작업(저장, 조회, 삭제, 업데이트)이 User 엔티티에서도 적용되게한다.
+    //fetch = FetchType.LAZY는 User 엔티티를 로드할 때 Page 엔티티를 즉시 로드하지 않고, 필요할 때 로드되도록 설정한다.
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Page myPage;
 
     @ManyToMany(mappedBy = "users")
     private List<Team> teams;
