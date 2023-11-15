6c8467a9000c6a1015120b37faa9c1dd65288956
David <isdnhk@naver.com>
2023-11-15 11:03:31 +0900

    [add] Team에 조인 요청을 보낸 User를 관리하는 pendingUsers추가

--- src/main/java/com/example/bilda_server/domain/Team.java
+++ src/main/java/com/example/bilda_server/domain/Team.java
@@ -5,11 +5,15 @@ import com.example.bilda_server.domain.RecruitmentStatus;
 import com.example.bilda_server.domain.Subject;
 import com.example.bilda_server.domain.User;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -40,5 +44,18 @@ public class Team {
 
     private String teamInfoMessage;
     private LocalDate buildStartDate;
+
+
+    //pending은 보류중이라는 뜻이다.
+    //생성된 팀에 대해서 참가신청을 보낸 유저들을 관리하기 위한 list이다.
+    //teamLeader는 리스트를 검토하여 사용자의 join여부를 결정한다. 승인된 경우는 users리스트로 이동한다.
+    @ManyToMany
+    @JoinTable(
+            name = "pending_user_team",
+            joinColumns = @JoinColumn(name = "team_id"),
+            inverseJoinColumns =  @JoinColumn(name ="user_id")
+    )
+
+    private List<User> pendingUsers;
 
 }
