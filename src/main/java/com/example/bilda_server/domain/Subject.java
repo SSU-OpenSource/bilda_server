e9edfad9f3527982c65d9c04d6644859625fca4d
David <isdnhk@naver.com>
2023-11-16 11:03:24 +0900

    [update] subject 도메인 업데이트

--- src/main/java/com/example/bilda_server/domain/Subject.java
+++ src/main/java/com/example/bilda_server/domain/Subject.java
@@ -1,22 +1,21 @@
 package com.example.bilda_server.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
 public class Subject {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long subjectCode;
 
+    @Enumerated(EnumType.STRING)
     private Department department;
 
     private String professor;
-    private LocalDate startDate;
-    private LocalDate endDate;
     private String section; //학기 정보
 }
