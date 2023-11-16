d41ac1f5204f6793999dc6d4b38f946180ce3b76
gundorit <packdev937@gmail.com>
2023-11-16 19:55:42 +0900

    refactor (RequestURI) : ConstantUtils를 RequestURI로 리팩토링 한다

--- src/main/java/com/example/bilda_server/utils/ConstantUtils.java
+++
@@ -1,9 +0,0 @@
-package com.example.bilda_server.utils;
-
-public final class ConstantUtils {
-
-    public static final String URL_PREFIX = "/api/v1";
-
-    private ConstantUtils() {
-    }
-}
---
+++ src/main/java/com/example/bilda_server/utils/RequestURI.java
@@ -0,0 +1,9 @@
+package com.example.bilda_server.utils;
+
+public final class RequestURI {
+
+    public static final String USER_REQUEST_PREFIX = "/api/v1/users";
+
+    private RequestURI() {
+    }
+}
