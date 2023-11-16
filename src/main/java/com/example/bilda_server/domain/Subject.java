27375ded9b65f6f9e89a56b90d93fb8cccef44a5
David <isdnhk@naver.com>
2023-11-16 14:42:16 +0900

    [update] Subject의 학과 정보가 단일 학과가 아닌 다수의 학과가 될 수 있게 수정

--- src/main/java/com/example/bilda_server/domain/Subject.java
+++ src/main/java/com/example/bilda_server/domain/Subject.java
@@ -1,21 +1,32 @@
 package com.example.bilda_server.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Subject {
     @Id
     private Long subjectCode;
 
+    private String title;
+
+    @ElementCollection(targetClass = Department.class)
     @Enumerated(EnumType.STRING)
-    private Department department;
+    @CollectionTable(name = "subject_department", joinColumns = @JoinColumn(name = "subject_code"))
+    @Column(name = "department")
+    private Set<Department> departments;
 
     private String professor;
     private String section; //학기 정보
+
 }
