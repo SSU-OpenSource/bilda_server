7acd45e9fc6c517352919bfe5445ff7132becc7a
gundorit <packdev937@gmail.com>
2023-11-20 00:36:20 +0900

    feat (SecurityConfig) : SecurityConfig 설정을 추가한다

--- src/main/java/com/example/bilda_server/config/SecurityConfig.java
+++ src/main/java/com/example/bilda_server/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.example.bilda_server.config;
 
+import com.example.bilda_server.utils.RequestURI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,12 +21,13 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.csrf(CsrfConfigurer::disable);
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(
+	auth -> auth.requestMatchers(RequestURI.USER_REQUEST_PREFIX + "/login").permitAll()
+	    .anyRequest().authenticated());
 
-        http.authorizeHttpRequests(authorize ->
-                authorize.requestMatchers("/api/v1/subject/**").permitAll()
-        );
         return http.build();
     }
 }
