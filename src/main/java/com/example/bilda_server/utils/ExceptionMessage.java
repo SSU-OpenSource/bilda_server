589d6a66ddb816e7dc650a8241f1536a9d3cc9f6
gundorit <packdev937@gmail.com>
2023-11-20 01:25:43 +0900

    feat (ExceptionMessage) : 예외 처리 메세지들을 모아 놓는 상수 인터페이스를 생성한다

---
+++ src/main/java/com/example/bilda_server/utils/ExceptionMessage.java
@@ -0,0 +1,9 @@
+package com.example.bilda_server.utils;
+
+public final class ExceptionMessage {
+
+    public static final String USER_NOT_FOUND_BY_EMAIL = "해당 이메일로 가입된 계정이 없습니다.";
+
+    private ExceptionMessage() {
+    }
+}
