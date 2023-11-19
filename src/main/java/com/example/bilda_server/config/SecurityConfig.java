8c0f5926e00e7c3e6973e9c796535652bd0516be
gundorit <packdev937@gmail.com>
2023-11-20 01:07:44 +0900

    feat (SecurityConfig) : UsernamePasswordAuthenticationFilter 대신 CustomUsernamePasswordAuthenticationFilter를 추가한다

--- src/main/java/com/example/bilda_server/config/SecurityConfig.java
+++ src/main/java/com/example/bilda_server/config/SecurityConfig.java
@@ -1,16 +1,15 @@
 package com.example.bilda_server.config;
 
+import com.example.bilda_server.jwt.filter.CustomUsernamePasswordAuthenticationFilter;
 import com.example.bilda_server.utils.RequestURI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -26,7 +25,9 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(
 	auth -> auth.requestMatchers(RequestURI.USER_REQUEST_PREFIX + "/login").permitAll()
-	    .anyRequest().authenticated());
+	    .anyRequest().authenticated())
+            .addFilterAt(CustomUsernamePasswordAuthenticationFilter.getInstance(),
+	UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
