3e496cee8be6b1fba8c324017b8a13c8b0be104c
gundorit <packdev937@gmail.com>
2023-11-19 22:52:51 +0900

    hotfix (repository) : UserRepository 삭제 및 JPARepository 추가한다

--- src/main/java/com/example/bilda_server/repository/UserJpaRepository.java
+++ src/main/java/com/example/bilda_server/repository/UserJpaRepository.java
@@ -1,10 +1,12 @@
 package com.example.bilda_server.repository;
 
 import com.example.bilda_server.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserJpaRepository extends JpaRepository<User, Long>, UserRepository {
+public interface UserJpaRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByEmail(String email);
 }
