1e4e6df73d7a5f41f3cccb3d4f5242d33344b852
gundorit <packdev937@gmail.com>
2023-11-22 14:24:10 +0900

    feat : SecurityConfig를 수정한다

--- src/main/java/com/example/bilda_server/utils/ExceptionMessage.java
+++ src/main/java/com/example/bilda_server/utils/ExceptionMessage.java
@@ -3,6 +3,13 @@ package com.example.bilda_server.utils;
 public final class ExceptionMessage {
 
     public static final String USER_NOT_FOUND_BY_EMAIL = "해당 이메일로 가입된 계정이 없습니다.";
+    public static final String USER_ALREADY_EXIST = "이미 해당 이메일로 가입된 계정이 있습니다.";
+    public static final String LOGIN_FAILURE = "해당 이메일 또는 비밀번호를 잘못 입력하셨습니다.";
+    public static final String EXPIRED_PERIOD_REFRESH_TOKEN = "기한이 만료된 RefreshToken입니다.";
+    public static final String EXPIRED_PERIOD_ACCESS_TOKEN = "기한이 만료된 AccessToken입니다.";
+    public static final String INVALID_REFRESH_TOKEN = "올바르지 않은 형식의 RefreshToken입니다.";
+    public static final String INVALID_ACCESS_TOKEN = "올바르지 않은 형식의 AccessToken입니다.";
+
 
     private ExceptionMessage() {
     }
--- src/main/java/com/example/bilda_server/utils/RequestURI.java
+++ src/main/java/com/example/bilda_server/utils/RequestURI.java
@@ -2,8 +2,11 @@ package com.example.bilda_server.utils;
 
 public final class RequestURI {
 
-    public static final String USER_REQUEST_PREFIX = "/api/v1/users";
-    public static final String SUBJECT_REQUEST_PREFIX = "/api/v1/subject";
+    private static final String apiVersion = "/api/v1/";
+
+    public static final String USER_REQUEST_PREFIX = apiVersion + "users";
+    public static final String SUBJECT_REQUEST_PREFIX = apiVersion + "subject";
+    public static final String EMAIL_REQUEST_PREFIX = apiVersion + "email";
 
     private RequestURI() {
     }
