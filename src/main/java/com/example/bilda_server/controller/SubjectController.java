2b7244fee215f87445d14256b9b19c7958d62485
David <isdnhk@naver.com>
2023-11-24 21:09:25 +0900

    [feat] SubjectController에  baseResponse작성

--- src/main/java/com/example/bilda_server/controller/SubjectController.java
+++ src/main/java/com/example/bilda_server/controller/SubjectController.java
@@ -3,9 +3,16 @@ package com.example.bilda_server.controller;
 
 import com.example.bilda_server.domain.entity.Subject;
 import com.example.bilda_server.domain.entity.User;
+import com.example.bilda_server.response.AuthorizedResponse;
+import com.example.bilda_server.response.BaseResponse;
 import com.example.bilda_server.response.SubjectWithTeamStatusDTO;
+import com.example.bilda_server.response.UserSubjectDTO;
 import com.example.bilda_server.service.SubjectService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -23,6 +30,13 @@ public class SubjectController {
     private final SubjectService subjectService;
 
     @Operation(summary = "유저가 속해있는 학과에 개설된 과목정보 가져오기", description = "userId를 pathVariable로 넘기면 유저가 속해있는 과에 개설된 과목정보를 가져올 수 있습니다.  ", tags = {"SubjectController"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(schema = @Schema(implementation = List.class))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @GetMapping("/department/{userId}")
     @ResponseBody
     public ResponseEntity<List<Subject>> getSubjectsByDepartment(@PathVariable Long userId) {
@@ -31,6 +45,13 @@ public class SubjectController {
     }
 
     @Operation(summary = "유저가 속해있는 과목정보 가져오기", description = "userId를 pathVariable로 넘기면 유저가 듣고있는 과목정보를 가져올 수 있습니다.  ", tags = {"SubjectController"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(schema = @Schema(implementation = List.class))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @GetMapping("/{userId}")
     @ResponseBody
     public ResponseEntity<List<SubjectWithTeamStatusDTO>> getSubjects(@PathVariable Long userId) {
@@ -39,10 +60,17 @@ public class SubjectController {
     }
 
     @Operation(summary = "유저가 듣고 있는 과목 추가하기", description = "userId와 SubjectId를 pathVariable로 넘기면 유저가 듣고 있는 과목을 추가할 수 있습니다.  ", tags = {"SubjectController"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(schema = @Schema(implementation = UserSubjectDTO.class))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @PostMapping("/{userId}/add/{subjectCode}")
-    public ResponseEntity<User> addSubjectToUser(@PathVariable Long userId, @PathVariable Long subjectCode) {
-        subjectService.addUserToSubject(subjectCode, userId);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<BaseResponse<UserSubjectDTO>> addSubjectToUser(@PathVariable Long userId, @PathVariable Long subjectCode) {
+        UserSubjectDTO userSubjectDTO = subjectService.addUserToSubject(subjectCode, userId);
+        return ResponseEntity.ok(new BaseResponse<>(200, "유저의 과목 추가 성공", userSubjectDTO));
     }
 
 }
