42cc63e08f15b9bc545b6ed46d8b89fa417ae1e7
gundorit <packdev937@gmail.com>
2023-11-25 03:27:33 +0900

    feat : 모든 컨트롤러의 응답에 ResponseDto를 적용한다

--- src/main/java/com/example/bilda_server/controller/EmailController.java
+++ src/main/java/com/example/bilda_server/controller/EmailController.java
@@ -2,12 +2,24 @@ package com.example.bilda_server.controller;
 
 import static com.example.bilda_server.utils.RequestURI.*;
 
+import com.example.bilda_server.request.VerifyAuthCodeRequest;
+import com.example.bilda_server.request.VerifyEmailRequest;
+import com.example.bilda_server.response.AuthorizedResponse;
+import com.example.bilda_server.response.ResponseDto;
 import com.example.bilda_server.service.EmailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,25 +32,31 @@ public class EmailController {
 
     private final EmailService emailService;
 
+    @Operation(summary = "이메일 전송", description = "인증을 위한 이메일을 전송합니다. 인증 코드 ㅇ비력 후 /verify/code 요청을 보내면 유효한 코드 인지 알 수 있습니다.")
+    @Parameter(name = "email", description = "인증 코드를 보낼 이메일 주소 입니다.", in = ParameterIn.QUERY)
     @GetMapping("/verify")
-    public ResponseEntity verify(@RequestParam String email) throws RuntimeException {
+    public ResponseDto<Void> verify(@RequestBody VerifyEmailRequest verifyEmailRequest)
+        throws RuntimeException {
         try {
-            emailService.sendVerificationMail(email);
-            return new ResponseEntity(HttpStatus.OK);
+            emailService.sendVerificationMail(verifyEmailRequest.email());
+            return ResponseDto.success();
         } catch (NotFoundException e) {
-            return new ResponseEntity(HttpStatus.NO_CONTENT);
+            return ResponseDto.fail(HttpStatus.BAD_REQUEST, e.getMessage());
         }
     }
 
+    @Operation(summary = "이메일 인증", description = "이메일로부터 전송받은 인증 코드를 인증합니다. result 값이 true면 인증이 완료된 것입니다.")
+    @Parameter(name = "email", description = "인증 코드를 받은 email 주소")
+    @Parameter(name = "authCode", description = "인증 코드")
     @GetMapping("/verify/code")
-    public ResponseEntity getVerify(@RequestParam String email, @RequestParam String authCode)
+    public ResponseDto<Void> getVerify(@RequestBody VerifyAuthCodeRequest verifyAuthCodeRequest)
         throws NotFoundException {
         try {
-            emailService.verifyEmail(email, authCode);
-            HttpHeaders httpHeaders = new HttpHeaders();
-            return new ResponseEntity<>(httpHeaders, HttpStatus.OK);
+            emailService.verifyEmail(verifyAuthCodeRequest.email(),
+	verifyAuthCodeRequest.authCode());
+            return ResponseDto.success();
         } catch (Exception e) {
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            return ResponseDto.fail(HttpStatus.BAD_REQUEST, e.getMessage());
         }
     }
 }
--- src/main/java/com/example/bilda_server/controller/TeamController.java
+++ src/main/java/com/example/bilda_server/controller/TeamController.java
@@ -3,6 +3,7 @@ package com.example.bilda_server.controller;
 import com.example.bilda_server.domain.entity.Team;
 import com.example.bilda_server.request.CreateTeamRequest;
 import com.example.bilda_server.response.PendingUserDTO;
+import com.example.bilda_server.response.ResponseDto;
 import com.example.bilda_server.response.TeamResponseDTO;
 import com.example.bilda_server.response.TeamsOfSubjectDTO;
 import com.example.bilda_server.service.TeamService;
@@ -22,94 +23,103 @@ public class TeamController {
 
     private final TeamService teamService;
 
-    @Operation(summary = "팀의 정보 가져오기", description = "teamId를 pathVariable로 넘기면 해당 팀에 대한 정보가 나옵니다. ", tags = {"TeamController"})
+    @Operation(summary = "팀의 정보 가져오기", description = "teamId를 pathVariable로 넘기면 해당 팀에 대한 정보가 나옵니다. ", tags = {
+        "TeamController"})
     @GetMapping("/{teamId}")
     @ResponseBody
-    public ResponseEntity<TeamResponseDTO> getTeam(
-            @PathVariable Long teamId
+    public ResponseDto<TeamResponseDTO> getTeam(
+        @PathVariable Long teamId
     ) {
 
         TeamResponseDTO team = teamService.findTeam(teamId);
-        return ResponseEntity.ok(team);
+        return ResponseDto.success("팀 정보 조회 안료", team);
     }
 
-    @Operation(summary = "User가 속해있는 팀들의 정보를 가져옵니다.", description = "userId를 pathVariable로 넘기면 해당 팀들에 대한 정보가 나옵니다. ", tags = {"TeamController"})
+    @Operation(summary = "User가 속해있는 팀들의 정보를 가져옵니다.", description = "userId를 pathVariable로 넘기면 해당 팀들에 대한 정보가 나옵니다. ", tags = {
+        "TeamController"})
     @ResponseBody
     @GetMapping("/user/{userId}")
-    public ResponseEntity<List<TeamResponseDTO>> getTeams(
-            @PathVariable Long userId
+    public ResponseDto<List<TeamResponseDTO>> getTeams(
+        @PathVariable Long userId
     ) {
         List<TeamResponseDTO> teams = teamService.findTeamsByUserId(userId);
-        return ResponseEntity.ok(teams);
+        return ResponseDto.success("유저가 속해있는 팀 정보 조회 완료", teams);
     }
 
-    @Operation(summary = "과목에 해당하는 팀들의 정보 가져오기", description = "SubjectId를 pahtvariable로 넘기면 해당 과목에 대해 개설된 팀들의 정보가 나옵니다. recruitmentStatus로 분기 처리해주시면 됩니다. ", tags = {"TeamController"})
+    @Operation(summary = "과목에 해당하는 팀들의 정보 가져오기", description = "SubjectId를 pahtvariable로 넘기면 해당 과목에 대해 개설된 팀들의 정보가 나옵니다. recruitmentStatus로 분기 처리해주시면 됩니다. ", tags = {
+        "TeamController"})
     @ResponseBody
     @GetMapping("/subject/{subjectId}")
-    public ResponseEntity<List<TeamsOfSubjectDTO>> getTeamsBySubjectID(
-            @PathVariable Long subjectId
+    public ResponseDto<List<TeamsOfSubjectDTO>> getTeamsBySubjectID(
+        @PathVariable Long subjectId
     ) {
         List<TeamsOfSubjectDTO> teams = teamService.findTeamsBySubjectId(subjectId);
-        return ResponseEntity.ok(teams);
+        return ResponseDto.success("과목에 해당하는 팀 정보 조회 완료", teams);
     }
 
-    @Operation(summary = "팀 생성하기", description = "Http request body를 이용하여 리더가 팀을 생성할 수 있습니다. ", tags = {"TeamController"})
+    @Operation(summary = "팀 생성하기", description = "Http request body를 이용하여 리더가 팀을 생성할 수 있습니다. ", tags = {
+        "TeamController"})
     @PostMapping("/create/{leaderId}")
-    public ResponseEntity<Void> createTeam(
-            @PathVariable Long leaderId,
-            @RequestBody CreateTeamRequest request) {
+    public ResponseDto<Void> createTeam(
+        @PathVariable Long leaderId,
+        @RequestBody CreateTeamRequest request) {
 
         Team team = teamService.createTeam(leaderId, request);
-        return ResponseEntity.ok().build();
+        return ResponseDto.success();
     }
 
-    @Operation(summary = "팀 조인 요청 수락하기", description = "TeamId와 팀에 추가할 userId를 pathVariable로 넘기면 leader가 join요청을 수락할 수 있습니다. 조인을 수락했을 때 해당 팀의 인원수가 초기 설정한 max인원수와 같아지면 team의 모집 상태는 모집 완료로 바뀌게 됩니다.", tags = {"TeamController"})
+    @Operation(summary = "팀 조인 요청 수락하기", description = "TeamId와 팀에 추가할 userId를 pathVariable로 넘기면 leader가 join요청을 수락할 수 있습니다. 조인을 수락했을 때 해당 팀의 인원수가 초기 설정한 max인원수와 같아지면 team의 모집 상태는 모집 완료로 바뀌게 됩니다.", tags = {
+        "TeamController"})
     @PostMapping("{teamId}/approve/{pendingUserId}")
-    public ResponseEntity<Void> approveJoinRequest(
-            @PathVariable Long teamId,
-            @PathVariable Long pendingUserId
+    public ResponseDto<Void> approveJoinRequest(
+        @PathVariable Long teamId,
+        @PathVariable Long pendingUserId
     ) {
         teamService.approvePendingUser(teamId, pendingUserId);
-        return ResponseEntity.ok().build();
+        return ResponseDto.success();
     }
 
-    @Operation(summary = "팀 조인 요청 거절하기", description = "TeamId와 팀에 추가할 userId를 pathVariable로 넘기면 leader가 join요청을 거절할 수 있습니다. ", tags = {"TeamController"})
+    @Operation(summary = "팀 조인 요청 거절하기", description = "TeamId와 팀에 추가할 userId를 pathVariable로 넘기면 leader가 join요청을 거절할 수 있습니다. ", tags = {
+        "TeamController"})
     @PostMapping("{teamId}/reject/{pendingUserId}")
-    public ResponseEntity<Void> rejectJoinRequest(
-            @PathVariable Long teamId,
-            @PathVariable Long pendingUserId
+    public ResponseDto<Void> rejectJoinRequest(
+        @PathVariable Long teamId,
+        @PathVariable Long pendingUserId
     ) {
         teamService.rejectPendingUser(teamId, pendingUserId);
-        return ResponseEntity.ok().build();
+        return ResponseDto.success();
     }
 
 
-    @Operation(summary = "팀에 조인 요청하기", description = "TeamId와 팀에 추가할 userId를 pathVariable로 넘기면 해당 팀에 사용자가 조인 요청을 할 수 있습니다. ", tags = {"TeamController"})
+    @Operation(summary = "팀에 조인 요청하기", description = "TeamId와 팀에 추가할 userId를 pathVariable로 넘기면 해당 팀에 사용자가 조인 요청을 할 수 있습니다. ", tags = {
+        "TeamController"})
     @PostMapping("/{teamId}/join/{userId}")
-    public ResponseEntity<Void> requestJoinTeam(
-            @PathVariable Long teamId,
-            @PathVariable Long userId
+    public ResponseDto<Void> requestJoinTeam(
+        @PathVariable Long teamId,
+        @PathVariable Long userId
     ) {
         teamService.addPendingUserToTeam(teamId, userId);
-        return ResponseEntity.ok().build();
+        return ResponseDto.success();
     }
 
-    @Operation(summary = "팀 조인 요청 확인하기", description = "TeamId를 pathVariable로 넘기면 해당 팀에 조인 요청을 보낸 사용자들이 불러와집니다 ", tags = {"TeamController"})
+    @Operation(summary = "팀 조인 요청 확인하기", description = "TeamId를 pathVariable로 넘기면 해당 팀에 조인 요청을 보낸 사용자들이 불러와집니다 ", tags = {
+        "TeamController"})
     @GetMapping("/{teamId}/recruit")
-    public ResponseEntity<List<PendingUserDTO>> getPendingUsersByTeamId(
-            @PathVariable Long teamId
+    public ResponseDto<List<PendingUserDTO>> getPendingUsersByTeamId(
+        @PathVariable Long teamId
     ) {
 
         List<PendingUserDTO> pendingUsers = teamService.getPendingUsers(teamId);
-        return ResponseEntity.ok(pendingUsers);
+        return ResponseDto.success("팀 종인 요청 확인", pendingUsers);
     }
 
-    @Operation(summary = "팀플 종료하기", description = "TeamId를 pathVariable로 넘기면 팀의 completeStatus는 COMPLETE로 바뀌게 됩니다. ", tags = {"TeamController"})
+    @Operation(summary = "팀플 종료하기", description = "TeamId를 pathVariable로 넘기면 팀의 completeStatus는 COMPLETE로 바뀌게 됩니다. ", tags = {
+        "TeamController"})
     @PostMapping("/complete/{teamId}")
-    public ResponseEntity<Void> completeTeam(
-            @PathVariable Long teamId
-    ){
+    public ResponseDto<Void> completeTeam(
+        @PathVariable Long teamId
+    ) {
         teamService.setCompleteStatus(teamId);
-        return ResponseEntity.ok().build();
+        return ResponseDto.success();
     }
 }
