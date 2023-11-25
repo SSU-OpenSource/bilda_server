964415dc10b4da958602a4f992deee7c79d4a32f
David <isdnhk@naver.com>
2023-11-26 00:18:24 +0900

    [feat] User 데이터 초기화 삭제

--- src/main/java/com/example/bilda_server/config/DataInitializer.java
+++ src/main/java/com/example/bilda_server/config/DataInitializer.java
@@ -38,19 +38,15 @@ public class DataInitializer {
         subjectRepository.save(subject1);
         subjectRepository.save(subject2);
 
-        User user1 = new User(1L, "happy@gmail.com", "super1234!", "홍길동", "gildong", "20230001",
+        /*User user1 = new User(1L, "happy@gmail.com", "super1234!", "홍길동", "gildong", "20230001",
             Department.COMPUTER, new Page(), null, null, Role.USER);
         user1.getMyPage().setUser(user1);
         User user2 = new User(2L, "test@gmail.com", "super1234!", "테스트", "test", "20230002",
             Department.COMPUTER, new Page(), null, null, Role.USER);
         user2.getMyPage().setUser(user2);
-        User user4 = new User(4L, "testo@gmail.com", "super12234!", "철수", "chulsoo", "20230003",
-                Department.COMPUTER, new Page(), null, null, Role.USER);
-        user4.getMyPage().setUser(user4);
 
         userRepository.save(user1);
-        userRepository.save(user2);
-        userRepository.save(user4);
+        userRepository.save(user2);*/
 
     }
 
