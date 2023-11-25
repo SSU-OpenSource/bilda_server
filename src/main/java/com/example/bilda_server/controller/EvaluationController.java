f332ad30b5eeaea4b6ea00c9f533395c98ffa753
David <isdnhk@naver.com>
2023-11-25 21:18:29 +0900

    [feat] 평가 생성하기 기능

--- src/main/java/com/example/bilda_server/controller/EvaluationController.java
+++ src/main/java/com/example/bilda_server/controller/EvaluationController.java
@@ -1,4 +1,30 @@
 package com.example.bilda_server.controller;
 
+
+import com.example.bilda_server.request.EvaluationRequestDTO;
+import com.example.bilda_server.response.ResponseDto;
+import com.example.bilda_server.service.EvaluationService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/evaluation")
 public class EvaluationController {
+
+    private final EvaluationService evaluationService;
+
+    @Operation(summary = "평가 생성하기", description = "Http request body를 이용하여 평가를 생성할 수 있습니다. ", tags = {
+            "EvaluationController"})
+    @PostMapping("/create")
+    public ResponseDto<Void> createEvaluation(@RequestBody EvaluationRequestDTO evaluationDTO) {
+        evaluationService.createAndReflectEvaluation(evaluationDTO);
+        return ResponseDto.success("평가 반영 완료");
+    }
+
+
 }
--- src/main/java/com/example/bilda_server/service/EvaluationService.java
+++ src/main/java/com/example/bilda_server/service/EvaluationService.java
@@ -1,8 +1,47 @@
 package com.example.bilda_server.service;
 
 
+import com.example.bilda_server.domain.entity.Evaluation;
+import com.example.bilda_server.domain.entity.Page;
+import com.example.bilda_server.domain.entity.User;
+import com.example.bilda_server.repository.EvaluationRepository;
+import com.example.bilda_server.repository.PageRepository;
+import com.example.bilda_server.repository.UserJpaRepository;
+import com.example.bilda_server.request.EvaluationRequestDTO;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class EvaluationService {
+
+    private final EvaluationRepository evaluationRepository;
+    private final UserJpaRepository userRepository;
+    private final PageRepository pageRepository;
+
+    @Transactional
+    public void createAndReflectEvaluation(EvaluationRequestDTO evaluationRequestDTO) {
+        User evaluatedUser = userRepository.findById(evaluationRequestDTO.getEvaluatedUserId())
+                .orElseThrow(() -> new EntityNotFoundException("Evaluated User not found"));
+        User evaluatorUser = userRepository.findById(evaluationRequestDTO.getEvaluatorUserId())
+                .orElseThrow(() -> new EntityNotFoundException("Evaluator User not found"));
+
+        Page evaluatedUserPage = evaluatedUser.getMyPage();
+        if (evaluatedUserPage == null) {
+            throw new RuntimeException("Evaluated User's page not found");
+        }
+
+        Evaluation evaluation = new Evaluation();
+        evaluation.setEvaluatedUser(evaluatedUser);
+        evaluation.setEvaluator(evaluatorUser);
+        evaluation.setScores(evaluationRequestDTO.getScores());
+
+        evaluationRequestDTO.getScores().forEach(evaluatedUserPage::addEvaluationScore);
+
+        pageRepository.save(evaluatedUserPage);
+
+    }
+
 }
