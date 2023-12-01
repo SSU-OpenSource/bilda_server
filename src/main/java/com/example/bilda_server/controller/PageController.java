e3c84319a84556599c3ec953a927cbf7ab5779a4
David <isdnhk@naver.com>
2023-12-01 09:12:02 +0900

    [hotfix] 팀원의 userId를 이용하여 팀원의 마이 페이지 조회

--- src/main/java/com/example/bilda_server/controller/PageController.java
+++ src/main/java/com/example/bilda_server/controller/PageController.java
@@ -20,11 +20,18 @@ public class PageController {
 
     private final PageService pageService;
 
-    @Operation(summary = "마이페이지 조회", description = "유저의 id를 pathVariable로 넘기면 마이페이지의 정보들을 조회할 수 있습니다. ", tags = {
+    @Operation(summary = "마이페이지 조회", description = "마이페이지의 정보들을 조회할 수 있습니다. ", tags = {
             "PageController"})
     @GetMapping("")
     public PageAverageDTO getPageByUserId(@AuthenticationPrincipal CustomUserDetails userDetails) {
         return pageService.getAverageScores(userDetails.getId());
+    }
+
+    @Operation(summary = "팀원의 마이페이지 조회", description = "조회하고자하는 user의 id를 pathVariable로 넘기면 마이페이지의 정보들을 조회할 수 있습니다. ", tags = {
+            "PageController"})
+    @GetMapping("/{userId}")
+    public PageAverageDTO getPageByUserId(@PathVariable Long userId) {
+        return pageService.getAverageScores(userId);
     }
 
 
