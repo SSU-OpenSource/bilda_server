7c55132306edf36978b92bf87ff0ad74372c8aaa
gundorit <packdev937@gmail.com>
2023-12-14 23:54:14 +0900

    feat : 최신화를 진행한다

--- src/main/java/com/example/bilda_server/utils/RequestURI.java
+++ src/main/java/com/example/bilda_server/utils/RequestURI.java
@@ -2,8 +2,7 @@ package com.example.bilda_server.utils;
 
 public final class RequestURI {
 
-    private static final String apiVersion = "/api/v1/";
-
+    public static final String apiVersion = "/api/v1/";
     public static final String USER_REQUEST_PREFIX = apiVersion + "users";
     public static final String SUBJECT_REQUEST_PREFIX = apiVersion + "subject";
     public static final String EMAIL_REQUEST_PREFIX = apiVersion + "email";
